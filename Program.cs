6be7c5b9ea3c8c4b5898a8af8a6bd48d82a1d820
ahmad.alshawakri@socuimtech.com <ahmad@ahmad-alshawakri-pc.scoiumtech.local>
2023-05-23 18:25:12 +0300

    Main Method

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace AsyncProcessing
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            List<string> items = new List<string>()
+            {
+                "item#1",
+                "item#2",
+                "item#3",
+                "item#4",
+                "item#5",
+                "item#6",
+                "item#7",
+            };
+
+            List<Task<string>> tasks = items
+                .Select(item => Task.Run(() => ProcessItemAsync(item)))
+                .ToList();
+
+            await Task.WhenAll(tasks);
+
+            foreach (var task in tasks)
+            {
+                Console.WriteLine(task.Result);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static async Task<string> ProcessItemAsync(string item)
+        {
+            char charToRemove = '#';
+            int charToRemoveIndex = item.IndexOf(charToRemove);
+            string processedItem = $"{item.Remove(charToRemoveIndex, 1)} Processed";
+
+            await Task.Delay(1000);
+
+            return processedItem;
+        }
+    }
+}
